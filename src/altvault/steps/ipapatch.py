097d1ce39dddad2ee2c9b42c3415da186c7fc576
altvault <altvault>
2026-06-20 19:31:50 +0700

    add ipapatch step

---
+++ src/altvault/steps/ipapatch.py
@@ -0,0 +1,30 @@
+import subprocess
+from dataclasses import dataclass
+from pathlib import Path
+
+from altvault.steps.base import Step, StepResult
+
+
+@dataclass(frozen=True)
+class IpapatchStep(Step):
+    def run(self, context):
+        step_path: Path = context.work_dir / "ipapatch"
+        step_path.mkdir()
+        input_ipa_path: Path = context.current_ipa_path
+        final_ipa_path: Path = step_path / "final.ipa"
+
+        subprocess.run(
+            [
+                "ipapatch",
+                "--input",
+                input_ipa_path,
+                "--output",
+                final_ipa_path,
+                "--noconfirm",
+            ],
+            check=True,
+            cwd=step_path,
+        )
+
+        context.current_ipa_path = final_ipa_path
+        context.step_results.append(StepResult(name="ipapatch"))
