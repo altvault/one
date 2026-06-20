7de570c5862b8c9d61fe42dacc40855d19261c4f
altvault <altvault>
2026-06-20 14:38:42 +0700

    add cydia downloader

---
+++ src/altvault/download/cydia.py
@@ -0,0 +1,68 @@
+import gzip
+import hashlib
+from dataclasses import dataclass
+from os.path import basename
+from urllib.parse import urljoin, urlparse
+
+import httpx
+from debian.deb822 import Packages
+
+from altvault.download.base import DownloadFile
+from altvault.steps.base import Context, FileInfo
+from altvault.version import safe_version
+
+
+def _fetch_cydia_repo(url: str) -> list[Packages]:
+    packages_gz_url = f"{url}/Packages.gz"
+    response = httpx.get(packages_gz_url)
+    content = gzip.decompress(response.content).decode("utf-8")
+    packages = list(Packages.iter_paragraphs(content))
+    return packages
+
+
+@dataclass(frozen=True)
+class CydiaRepoFile(DownloadFile):
+    repo: str
+    package: str
+    architecture: str
+    version: str = "latest"
+
+    def download(self, context: Context) -> FileInfo:
+        all_packages = _fetch_cydia_repo(self.repo)
+        filtered_packages = [
+            pkg
+            for pkg in all_packages
+            if pkg["Package"] == self.package
+            and pkg["Architecture"] == self.architecture
+        ]
+        if self.version == "latest":
+            selected_package = max(
+                filtered_packages, key=lambda p: safe_version(p["Version"])
+            )
+        else:
+            selected_package = next(
+                (p for p in filtered_packages if p["Version"] == self.version), None
+            )
+        if not selected_package:
+            raise ValueError("Not found")
+        if self.use_version:
+            context.tweak_version_label = selected_package["Version"]
+
+        download_url = urljoin(f"{self.repo}/", selected_package["Filename"])
+
+        with httpx.stream("GET", download_url, follow_redirects=True) as r:
+            file_path = context.work_dir / basename(urlparse(download_url).path)
+            sha256 = hashlib.sha256()
+            with open(file_path, "wb") as f:
+                for chunk in r.iter_bytes():
+                    f.write(chunk)
+                    sha256.update(chunk)
+
+        extracted_files = self.extract_deb_files(context, file_path)
+
+        return FileInfo(
+            path=file_path,
+            url=download_url,
+            sha256=sha256.hexdigest(),
+            extracted_files=extracted_files,
+        )
