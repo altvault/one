c5dd4518e2b2d9bfe3bb94b54b5af22d0f8a57e2
altvault <altvault>
2026-06-20 19:33:19 +0700

    add infuse recipe

---
+++ recipes/infuse.py
@@ -0,0 +1,27 @@
+from altvault.download.github import GitHubReleaseFile
+from altvault.recipe import Recipe, Tweak
+from altvault.steps.cyan import CyanStep
+from altvault.steps.downloadipa import DownloadIpaStep
+
+recipe = Recipe(
+    name="Infuse",
+    bundle_identifier="com.firecore.infuse",
+    tweaks=[
+        Tweak(
+            name="InfusePlus",
+            pipeline=[
+                DownloadIpaStep(),
+                CyanStep(
+                    download_files=[
+                        GitHubReleaseFile(
+                            owner="dayanch96",
+                            repo="InfusePlus",
+                            endswith="arm.deb",
+                            use_version=True,
+                        )
+                    ]
+                ),
+            ],
+        )
+    ],
+)
