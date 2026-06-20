52ae24ad4f2a9dd1b78edc0a1bb73f507d189667
altvault <altvault>
2026-06-20 19:41:10 +0700

    add youtubemusic recipe

---
+++ recipes/youtubemusic.py
@@ -0,0 +1,27 @@
+from altvault.download.github import GitHubReleaseFile
+from altvault.recipe import Recipe, Tweak
+from altvault.steps.cyan import CyanStep
+from altvault.steps.downloadipa import DownloadIpaStep
+
+recipe = Recipe(
+    name="YouTubeMusic",
+    bundle_identifier="com.google.ios.youtubemusic",
+    tweaks=[
+        Tweak(
+            name="YTMusicUltimate",
+            pipeline=[
+                DownloadIpaStep(),
+                CyanStep(
+                    download_files=[
+                        GitHubReleaseFile(
+                            owner="dayanch96",
+                            repo="YTMusicUltimate",
+                            endswith="arm.deb",
+                            use_version=True,
+                        )
+                    ]
+                ),
+            ],
+        )
+    ],
+)
