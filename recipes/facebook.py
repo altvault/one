5b1a04542f200b8d7f9a969812f861b1a964ebf9
altvault <altvault>
2026-06-20 19:28:07 +0700

    add facebook recipe

---
+++ recipes/facebook.py
@@ -0,0 +1,27 @@
+from altvault.download.github import GitHubReleaseFile
+from altvault.recipe import Recipe, Tweak
+from altvault.steps.cyan import CyanStep
+from altvault.steps.downloadipa import DownloadIpaStep
+
+recipe = Recipe(
+    name="Facebook",
+    bundle_identifier="com.facebook.Facebook",
+    tweaks=[
+        Tweak(
+            name="FacebookGlow",
+            pipeline=[
+                DownloadIpaStep(),
+                CyanStep(
+                    download_files=[
+                        GitHubReleaseFile(
+                            owner="dayanch96",
+                            repo="Glow",
+                            endswith="arm.deb",
+                            use_version=True,
+                        )
+                    ]
+                ),
+            ],
+        )
+    ],
+)
