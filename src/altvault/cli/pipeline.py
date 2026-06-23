34e9e593b0f3bbeec165b1b487b394801a98f045
altvault <altvault>
2026-06-23 23:27:08 +0700

    add pipeline cli command

--- src/altvault/cli/pipeline.py
+++ src/altvault/cli/pipeline.py
@@ -1,14 +1,32 @@
 import argparse
+import tempfile
+from pathlib import Path
+
+from altvault.github import create_github_client
+from altvault.recipes import get_tweak_recipe
+from altvault.steps.base import Context
 
 
 def register(subparsers: argparse._SubParsersAction):
     parser = subparsers.add_parser("pipeline")
-    parser.add_argument("name")
+    parser.add_argument("tweak_name")
     parser.add_argument("--app-version")
     return parser
 
 
 def run(args: argparse.Namespace):
-    print(args.name)
-    print(args.app_version)
-    pass
+    github_client = create_github_client()
+    recipe = get_tweak_recipe(name=args.tweak_name)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmpdir: Path = Path(tmpdirname)
+        context: Context = Context(
+            recipe=recipe.app,
+            tweak=recipe.tweak,
+            app_version=args.app_version,
+            github_client=github_client,
+            work_dir=tmpdir,
+        )
+
+        for step in recipe.tweak.pipeline:
+            step.run(context=context)
