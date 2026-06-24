e8c8535cc7f98f05c8a44c46edbe90b2dd1a6ef0
altvault <altvault>
2026-06-24 12:25:53 +0700

    worker: add query helper

---
+++ worker/src/query.ts
@@ -0,0 +1,73 @@
+import globals from "../../globals.json";
+
+type GithubSearchResponse = {
+  data?: {
+    search: {
+      nodes: Array<{
+        latestRelease: {
+          tagName: string;
+          releaseAssets: {
+            nodes: Array<{
+              name: string;
+              size: number;
+              createdAt: string;
+              url: string;
+            }>;
+          };
+          description: string;
+        };
+      }>;
+    };
+  } | null;
+  errors?: Array<{
+    message: string;
+    locations?: Array<{
+      line: number;
+      column: number;
+    }>;
+    path?: Array<string | number>;
+    extensions?: Record<string, unknown>;
+  }>;
+};
+
+const query = `query ($searchQuery: String!) {
+  search(query: $searchQuery, type: REPOSITORY, first: 100) {
+    nodes {
+      ... on Repository {
+        latestRelease {
+          tagName
+          releaseAssets(first: 100) {
+            nodes {
+              name
+              size
+              createdAt
+              url
+            }
+          }
+          description
+        }
+      }
+    }
+  }
+}`;
+
+const variables = {
+  searchQuery: `org:${globals.owner} topic:tweaked-ipas`,
+};
+
+export async function queryReleases(token: string) {
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+      "User-Agent": "Worker",
+    },
+    body: JSON.stringify({ query, variables }),
+  });
+  const result = (await response.json()) as GithubSearchResponse;
+  if (result.errors && result.errors.length > 0) {
+    throw new Error(result.errors[0].message);
+  }
+  return result;
+}
